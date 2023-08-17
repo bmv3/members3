55a9bf030adef6ad8acf16dd6c8dd353107e1fd8
Michael V. Bykov <bmv3@mail.ru>
2023-08-17 11:01:34 +0500

    First commit

---
+++ src/components/Heder.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Header({ setSortBy, checked }) {
+
+    const sortByChangeHabdler = (e) => {
+        setSortBy(e.target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <center>
+                    <p>Сортировать по:
+                        <label>
+                            <input
+                                name="sort"
+                                type="radio"
+                                onChange={e => sortByChangeHabdler(e)}
+                                value="sortByCity"
+                                checked={checked === 'sortByCity'}
+                            /> городу
+                        </label>
+                        <label>
+                            <input name="sort"
+                                type="radio"
+                                onChange={e => sortByChangeHabdler(e)}
+                                value="sortByName"
+                                checked={checked === 'sortByName'}
+                            /> имени
+                        </label>
+                    </p>
+                </center>
+            </form>
+        </div>
+    )
+}
---
+++ src/components/PartMembers.jsx
@@ -0,0 +1,26 @@
+import Member from "./Member";
+
+const PartMembers = ({ members, roles, filter }) => {
+    const currentMembers = members.filter((item) => item.role_category.indexOf(filter) >= 0);
+    return (
+        <div className="members">
+            {currentMembers.length > 0 ?
+                currentMembers.map(({ id, title, member_city, role_category, member_photo, member_link, both }) => <Member
+                    id={id}
+                    key={id.toString()}
+                    name={title.rendered}
+                    city={member_city}
+                    role={both === "1" ? "Обе роли" : roles[role_category]}
+                    photo={member_photo.guid}
+                    link={member_link}
+                    both={both}
+                />)
+                :
+                <p className="members-info">Вот-вот появятся.</p>
+            }
+
+        </div>
+    )
+}
+
+export default PartMembers;
